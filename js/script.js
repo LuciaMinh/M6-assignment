00d2c0badb1d2be59e395c7283c671d75ab30e93
Minh Nguyen <nguyenminh.2291@gmail.com>
2024-10-29 23:39:13 -0700

    commit 1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-window.addEventListener('load', () => {
-    const form = document.querySelector('empForm');
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const formData = {
-        id: document.getElementById('id').value,
-        name: document.getElementById('name').value,
-        ext: document.getElementById('ext').value,
-        email: document.getElementById('email').value,
-        department: document.getElementById('department').value,
-      };
-      console.log(formData);
-    });
-  });
+let id;
+let name;
+let ext;
+let email;
+let department;
+let submit;
+
+id = document.getElementById('id');
+fullName = document.getElementById('name');
+ext = document.getElementById('ext');
+email = document.getElementById('email');
+department = document.getElementById('department');
+submit = document.querySelector('button[type="submit"]');
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  const formData = {
+    '1. ID'              : id.value,
+    '2. Name'            : fullName.value,
+    '3. Extension'       : ext.value,
+    '4. Email'           : email.value,
+    '5. Department'      : department.value,
+  };
+  console.log(formData);
+});
